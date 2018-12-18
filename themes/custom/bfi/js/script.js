3bcf4765c3ab1d905e0b4fae7440f470b299b8a2
ckc1230 <chriskchan1230@gmail.com>
2018-12-17 21:10:04 -0800

    initial commit

---
+++ themes/custom/bfi/js/script.js
@@ -0,0 +1,17 @@
+AOS.init({
+  duration: 600,
+})
+
+
+$(document).ready(function() {
+  $('.subscribe-form').validate({
+    rules: {
+      firstName: "required",
+      lastName: "required",
+      email: {
+        required: true,
+        email: true
+      }
+    }
+  });
+});
